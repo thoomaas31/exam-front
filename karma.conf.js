94a51793257c0c0e5b0dbf8dd4d2ca38a6a51f32
Thomas Fourties <thomas.fourties@icloud.com>
2024-04-30 11:14:45 +0200

    config

--- karma.conf.js
+++ karma.conf.js
@@ -5,27 +5,25 @@ module.exports = function (config) {
         plugins: [
             require('karma-jasmine'),
             require('karma-chrome-launcher'),
-            require('karma-jasmine-html-reporter'),
-            require('karma-coverage'),
             require('@angular-devkit/build-angular/plugins/karma')
         ],
         client: {
-            clearContext: false
+            clearContext: false // leave Jasmine Spec Runner output visible in browser
         },
-        coverageReporter: {
+        jasmineHtmlReporter: {
+            suppressAll: true // removes the duplicated traces
+        },
+        coverageIstanbulReporter: {
             dir: require('path').join(__dirname, './coverage'),
-            subdir: '.',
-            reporters: [
-                { type: 'html' },
-                { type: 'lcovonly' }
-            ]
+            reports: ['html', 'lcovonly', 'text-summary'],
+            fixWebpackSourcePaths: true
         },
-        reporters: ['progress', 'kjhtml', 'coverage'],
+        reporters: ['progress', 'kjhtml'],
         port: 9876,
         colors: true,
         logLevel: config.LOG_INFO,
         autoWatch: true,
-        browsers: ['ChromeHeadless'],
+        browsers: ['ChromeHeadless'], // Utilisation de Chrome Headless
         singleRun: false,
         restartOnFileChange: true
     });
